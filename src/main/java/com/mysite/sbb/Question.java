04daef6cb0a3981d4285851090d190d9f545abbf
junseokPP <pjs981130@gmail.com>
2025-09-06 19:04:19 +0900

    1강 : QuestionRepository,TestInitData추가

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -1,11 +1,15 @@
 package com.mysite.sbb;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Question {
 
     @Id
