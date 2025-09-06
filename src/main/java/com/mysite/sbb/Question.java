f35634d5c82967263a86c128695e7f1caad02b66
junseokPP <pjs981130@gmail.com>
2025-09-06 16:28:54 +0900

    7강 : Question,Answer엔티티추가

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+}
