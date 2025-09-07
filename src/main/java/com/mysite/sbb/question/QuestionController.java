3cf72654b74a9d7b4c962c810bec6f80b5e5b2a7
junseokPP <pjs981130@gmail.com>
2025-09-07 15:26:16 +0900

    24강 : theach,thtext생성

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,8 +2,10 @@ package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
@@ -13,12 +15,18 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
 
     @GetMapping( "/question/list")
-    public String list() {
+    public String list(Model model) {
 
         String questions = questionRepository.findAll()
                 .stream()
                 .map(e -> "<li>%s</li>".formatted(e.getSubject()))
                 .collect(Collectors.joining("\n"));
+
+        String value = "홍길동";
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+
+        model.addAttribute("value", value);
+        model.addAttribute("list", list);
 
 
         return "question_list";
